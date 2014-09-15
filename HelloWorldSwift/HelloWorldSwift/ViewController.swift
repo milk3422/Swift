01c658d63ccbb3f149feda431854a5a646cc453a
Zachary Radtka <zachary.radtka@gmail.com>
2014-09-15 14:26:04 +0200

    ZER - A simple Iphone app to display a users name

--- HelloWorldSwift/HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/HelloWorldSwift/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var nameLabel : UILabel = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorldAction(sender : UITextField) {
+        nameLabel.text = "Hi \(sender.text) "
+    }
 
 }
 
