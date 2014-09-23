afedb124af8e01d6494e770d96e5170e981d8fb8
Zachary Radtka <zachary.radtka@gmail.com>
2014-09-23 09:19:26 +0200

    ZER - Added a file that outlines Swift basics

---
+++ SwiftBasics.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+// Comments can be single line comments
+
+/*
+*  Or they can be many
+*  many
+*  many lines
+*/
+
+
+var str = "Hello, playground"
+
