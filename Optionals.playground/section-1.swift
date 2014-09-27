008bc02dd2c8d4b487d8f6acbf9b9841d20c642f
Zachary Radtka <zachary.radtka@gmail.com>
2014-09-27 19:42:36 +0200

    ZER - Added a playground that uses optionals

---
+++ Optionals.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// This playground highlights the use of optionals
+
+// Any variable set as an optional is initialized as nil
+// nil is a sentinal value
+var optionalNum: Int?
+
+
